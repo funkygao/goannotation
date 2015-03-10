5b277c4aeb7798a37558a8cd839751bda2c5c719
funkygao <funky.gao@gmail.com>
2015-03-10 16:33:56 +0800

    godoc

---
+++ annotations/doc.go
@@ -0,0 +1,6 @@
+// Plugin based annotation mechanism.
+// When you want to add a new annotation tag, just put it inside annotations/.
+// The new annotation must implement the engine.Annotation interface.
+//
+// Refer to joiner.go as a sample annotation implementation.
+package annotations
