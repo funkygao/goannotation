12e114830b1799ca034fddac6f7590360551b722
Funky Gao <funky.gao@gmail.com>
2016-09-27 08:59:58 +0800

    fmt of @rest output

--- annotations/rest.go
+++ annotations/rest.go
@@ -3,13 +3,18 @@ package annotations
 import (
 	"fmt"
 	"go/ast"
+	"strings"
 
 	"github.com/funkygao/goannotation/engine"
+	"github.com/funkygao/golib/color"
 )
 
 var _ engine.FuncAnnotation = &Rest{}
 
 // Rest is an annotation implementation applied on a REST controller function.
+//
+// Format of @rest:
+// {method} {uri}
 type Rest struct {
 	attrs map[string]string
 }
@@ -24,7 +29,15 @@ func (this *Rest) SetAttrs(kv map[string]string) {
 
 func (this *Rest) Execute(pkgName string, funcName string, decl *ast.FuncDecl) {
 	if doc, ok := this.attrs["doc"]; ok {
-		fmt.Println(doc)
+		fmt.Printf("%s\n", funcName)
+
+		parts := strings.SplitN(doc, " ", 2)
+		if len(parts) == 1 {
+			fmt.Printf("    %s\n", color.Yellow("%8s", parts[0]))
+		} else {
+			fmt.Printf("    %s %s\n", color.Green("%8s", parts[0]), parts[1])
+
+		}
 	}
 }
 
